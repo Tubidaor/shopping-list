5c90c993e78f25fc01985c5a1fc901d5ad448d89
Juan Baltazar <juan.baltazar1@gmail.com>
2019-08-08 14:15:42 -0700

    added delete an checked

--- index.js
+++ index.js
@@ -21,5 +21,50 @@ function shoppingList () {
 }
 $(shoppingList);
 
+function shoppingList1 () {
+  if  ($('input[name="shopping-list-entry"]').keydown(event =>
+    $('.output').text(`Key pressed: ${event.key}`)) === 'Enter') {
+  
+  $('#js-shopping-list-form').submit( event => {
+  event.preventDefault();
+  let item = $(this).find('input[name="shopping-list-entry"]').val();
+  let itemPlace = 
+    $(`<li>
+        <span class="shopping-item"> ${item} </span>
+        <div class="shopping-item-controls">
+          <button class="shopping-item-toggle">
+            <span class="button-label">check</span>
+          </button>
+          <button class="shopping-item-delete">
+            <span class="button-label">delete</span>
+          </button>
+        </div>
+    </li>`);
+    $('.shopping-list').append(itemPlace); 
+    $("#shopping-list-entry").val("");
+
+  });
+}
+}
+$(shoppingList1);
+
+
+
+$(function deleteItem () {
+  $('.shopping-item-delete').on('click', function () {
+  let target = $(this).parent()
+  target.parent().remove()
+
+  });
+});
+
+
+$(function itemChecked () {
+  $('.shopping-item-toggle').on('click', function () {
+  let target = $(this).closest('div').siblings('span')
+  target.toggleClass('shopping-item__checked');
+
+  });
+});
 
 

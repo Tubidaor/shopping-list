908153ca7308d80d6b58034f76a2e4a0187659d7
Juan Baltazar <juan.baltazar1@gmail.com>
2019-08-07 22:02:12 -0700

    added code to append on button submit

---
+++ index.js
@@ -0,0 +1,32 @@
+/*function shoppingList () {
+  $('#js-shopping-list-form').submit( event => {
+  event.preventDefault();
+  let item = $(this).find('input[name="shopping-list-entry"]').val();
+  let itemPlace = 
+    $(`<li>
+        <span class="shopping-item"> ${item} </span>
+        <div class="shopping-item-controls">
+          <button class="shopping-item-toggle">
+            <span class="button-label">check</span>
+          </button>
+          <button class="shopping-item-delete">
+            <span class="button-label">delete</span>
+          </button>
+        </div>
+    </li>`);
+    $('.shopping-list').append(itemPlace); 
+
+  });
+}
+$(shoppingList);*/
+
+$(function shoppingList () {
+$("#js-shopping-list-form").submit( event => {
+  //event.preventDefault();
+ 
+
+  $('.shopping-list').append(`<div> hi </div>`); 
+});
+});
+
+

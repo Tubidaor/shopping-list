0f21a0f7c9e15ffc7e5220260e67f86836725c5a
Tubidaor <53272683+Tubidaor@users.noreply.github.com>
2019-08-07 22:09:22 -0700

    Update index.js

--- index.js
+++ index.js
@@ -1,4 +1,4 @@
-/*function shoppingList () {
+function shoppingList () {
   $('#js-shopping-list-form').submit( event => {
   event.preventDefault();
   let item = $(this).find('input[name="shopping-list-entry"]').val();
@@ -18,15 +18,9 @@
 
   });
 }
-$(shoppingList);*/
+$(shoppingList);
 
 $(function shoppingList () {
-$("#js-shopping-list-form").submit( event => {
-  //event.preventDefault();
- 
 
-  $('.shopping-list').append(`<div> hi </div>`); 
-});
-});
 
 
